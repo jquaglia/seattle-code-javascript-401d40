50d27e03da723afb0069b1fedbd9539a0cebea85
jquaglia <jqugali@gmail.com>
2021-03-31 10:13:58 -0700

    completed warm up

--- class-33/warm-up/src/lib/Tree.js
+++ class-33/warm-up/src/lib/Tree.js
@@ -10,15 +10,40 @@ class Tree {
 
   // @param callback {function}
   traverseIteratively(callback) {
+    let current = this.root;
+    let queue = [];
 
+    queue.unshift(current);
 
+    while (queue) {
+      current = queue.pop();
+      callback(current.value);
+
+      current.children.forEach(child => {
+        if (child) {
+          queue.unshift(child);
+        }
+      });
+    };
   }
 
   // @param callback {function}
   traverseRecursively(callback) {
+      let current = this.root;
+      const array = [];
 
-
+      const weirdTraverse = (node) => {
+        callback(node.value);
+        if (node.children) {
+          node.children.forEach(child => {
+            weirdTraverse(child);
+          });
+        }
+      };
+      weirdTraverse(current);
   }
 }
+
+
 
 module.exports = Tree;
